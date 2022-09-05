05b2a30437c3b47633b655b6b48da31e36b116e4
zelymm <dkdlwkrz@naver.com>
2022-09-05 14:22:24 +0900

    initial commit

---
+++ src/main/java/com/ll/exam/app10/App10Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app10/App10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
